2534d00185aed0f1643d1d14c5ce29dee8572aad
Rick Carlino <rick.carlino@gmail.com>
2021-02-27 14:32:01 -0600

    Hello?

---
+++ README.md
@@ -0,0 +1,3 @@
+# HTML 3.1 Paint
+
+A collaborative drawing app for the '90s.
---
+++ server.rb
@@ -0,0 +1,21 @@
+require "chunky_png"
+require "sinatra"
+
+MAP_SIZE = 320
+WHITE    = ChunkyPNG::Color::WHITE
+BLACK    = ChunkyPNG::Color::BLACK
+canvas = ChunkyPNG::Canvas.new(MAP_SIZE, MAP_SIZE, WHITE)
+canvas.border!(3)
+
+get "/" do
+  keys = params.keys.first
+  if keys.length == 1
+    string = keys.first[0,7]
+    x, y = string.split(",").map(&:to_i)
+    color = canvas.get_pixel(x,y) == WHITE ? BLACK : WHITE
+    canvas.set_pixel_if_within_bounds(x, y, color)
+  end
+
+  @image = canvas.to_data_url
+  erb :index
+end
